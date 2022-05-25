223b60210937a33ad802e8c965d0de056505d27f
Marcos Ribes <mlribes@gmail.com>
2022-05-25 17:49:29 -0300

    Tradução do PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,76 @@
-import { Component } from '@angular/core';
+import { PathLocationStrategy } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private primeNGConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primeNGConfig.setTranslation({
+      startsWith: 'Starts with',
+      contains: 'Contains',
+      notContains: 'Not contains',
+      endsWith: 'Ends with',
+      equals: 'Equals',
+      notEquals: 'Not equals',
+      noFilter: 'No Filter',
+      lt: 'Less than',
+      lte: 'Less than or equal to',
+      gt: 'Greater than',
+      gte: 'Greater than or equal to',
+      is: 'Is',
+      isNot: 'Is not',
+      before: 'Before',
+      after: 'After',
+      dateIs: 'Date is',
+      dateIsNot: 'Date is not',
+      dateBefore: 'Date is before',
+      dateAfter: 'Date is after',
+      clear: 'Limpar',
+      apply: 'Apply',
+      matchAll: 'Match All',
+      matchAny: 'Match Any',
+      addRule: 'Add Rule',
+      removeRule: 'Remove Rule',
+      accept: 'Yes',
+      reject: 'No',
+      choose: 'Choose',
+      upload: 'Upload',
+      cancel: 'Cancel',
+      dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+      dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+      dayNamesMin: ['Do', 'Se', 'Te', 'Qa', 'Qi', 'Se', 'Sa'],
+      monthNames: [
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro',
+      ],
+      monthNamesShort: ['Jan', 'Feb', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+      dateFormat: 'dd/mm/yy',
+      firstDayOfWeek: 0,
+      today: 'Hoje',
+      weekHeader: 'Wk',
+      weak: 'Weak',
+      medium: 'Medium',
+      strong: 'Strong',
+      passwordPrompt: 'Enter a password',
+      emptyMessage: 'No results found',
+      emptyFilterMessage: 'No results found',
+    });
+  }
+}
