3242d745e35e9096e525f03589427ce2be55fff0
Marcos Ribes <mlribes@gmail.com>
2022-06-07 16:09:51 -0300

    Nova URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'http://athena.varejopositivo.com.br/vendedor/api/',
+  baseUrl: 'https://topseller.varejopositivo.com.br/api/',
   production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   baseUrl: 'http://localhost:8080/',
-  //baseUrl: 'http://athena.varejopositivo.com.br/vendedor/api/',
   production: false,
 };
